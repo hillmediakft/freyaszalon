39f4fb32d1688f2a8574b7bf5c7fda43b48ca766
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2018-12-04 18:05:27 +0100

    admin_controller login es newsletter/send_newsletter_timelimit auth nelkul futhat

--- system/core/admin_controller.php
+++ system/core/admin_controller.php
@@ -5,10 +5,24 @@ class Admin_controller extends Controller {
     {
         parent::__construct();
 
-        // Authentikáció minden admin oldalra, kivéve a login
-        if($this->request->get_controller() != 'login'){
+
+        // Authentikáció minden admin oldalra, kivéve a login és a newsletter/send_newsletter_timelimit oldalt
+        if( (($this->request->get_controller() == 'newsletter') && ($this->request->get_action() == 'send_newsletter_timelimit')) || ($this->request->get_controller() == 'login')
+        )	
+        {
+			
+
+        } else {
             Auth::handleLogin();
         }
+
+
+/*
+        if (($this->request->get_controller() != 'login')) {
+            Auth::handleLogin();
+        }
+*/
+
     }
 }
 ?>
